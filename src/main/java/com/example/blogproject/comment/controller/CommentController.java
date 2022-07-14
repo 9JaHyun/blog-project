8f0b73ea934416791d43b8bafc5cc90414614326
9JaHyun <jahyunnine@gmail.com>
2022-07-14 19:49:24 +0900

    반환 형식 ResponseEntity로 변경

--- src/main/java/com/example/blogproject/comment/controller/CommentController.java
+++ src/main/java/com/example/blogproject/comment/controller/CommentController.java
@@ -7,6 +7,8 @@ import com.example.blogproject.comment.service.CommentService;
 import com.example.blogproject.comment.dto.CommentUpdateRequestDto;
 import com.example.blogproject.config.security.MemberDetailsImpl;
 import java.util.List;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -27,32 +29,46 @@ public class CommentController {
 
     // 댓글 목록 보기
     @GetMapping("/api/comment/{articlesId}")
-    public List<CommentResponseDto> showComments(@PathVariable Long articlesId) {
-        return commentService.showComments(articlesId);
+    public ResponseEntity<Result<List<CommentResponseDto>>> showComments(@PathVariable Long articlesId) {
+        return ResponseEntity.status(HttpStatus.OK)
+              .body(new Result<List<CommentResponseDto>>(commentService.showComments(articlesId)));
     }
 
     // 댓글 작성
     @PostMapping("/api/comment")
-    public void writeComment(@AuthenticationPrincipal MemberDetailsImpl memberDetails,
+    public ResponseEntity<String> writeComment(@AuthenticationPrincipal MemberDetailsImpl memberDetails,
           @RequestBody CommentSaveRequestDto dto) {
         System.out.println("테스트");
         dto.setUsername(memberDetails.getMember().getUsername());
         commentService.writeComment(dto);
+        return ResponseEntity.status(HttpStatus.OK)
+              .body("성공!");
     }
 
     // 댓글 수정
     @PutMapping("/api/comment")
-    public void updateComment(@AuthenticationPrincipal MemberDetailsImpl memberDetails,
+    public ResponseEntity<String> updateComment(@AuthenticationPrincipal MemberDetailsImpl memberDetails,
           @RequestBody CommentUpdateRequestDto dto) {
         dto.setUsername(memberDetails.getMember().getUsername());
         commentService.updateComment(dto);
+        return ResponseEntity.status(HttpStatus.OK)
+              .body("성공!");
     }
 
     // 댓글 삭제
     @DeleteMapping("/api/comment")
-    public void deleteComment(@AuthenticationPrincipal MemberDetailsImpl memberDetails,
+    public ResponseEntity<String> deleteComment(@AuthenticationPrincipal MemberDetailsImpl memberDetails,
           @RequestBody CommentDeleteRequestDto dto) {
         dto.setUsername(memberDetails.getMember().getUsername());
         commentService.deleteComment(dto);
+        return ResponseEntity.status(HttpStatus.OK)
+              .body("성공!");
+    }
+
+    private static class Result<T>{
+        private T data;
+        public Result(T data) {
+            this.data = data;
+        }
     }
 }
